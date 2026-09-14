2c1f7e2b3228a7a9a69d776a2beb0c083121ffab
Z1nK <llobesa@gmail.com>
2026-09-14 19:34:11 +0200

    feat: Implement left and right join functions for table records in db worflow example

--- src/app/db-worflow-example/main.cpp
+++ src/app/db-worflow-example/main.cpp
@@ -36,6 +36,38 @@ void printInnerJoin(const Table& left, const Table& right) {
   std::cout << '\n';
 }
 
+void printLeftJoin(const Table& left, const Table& right) {
+  std::cout << "LEFT JOIN A x B ON A.id = B.id\n";
+  std::cout << "id | name_a     | name_b\n";
+  std::cout << "---+------------+----------\n";
+  for (const auto& recordA : sortedRecords(left)) {
+    if (auto recordB = right.get(recordA.id)) {
+      std::cout << std::right << std::setw(2) << recordA.id << " | " << std::left << std::setw(10) << recordA.name
+                << " | " << recordB->name << '\n';
+    } else {
+      std::cout << std::right << std::setw(2) << recordA.id << " | " << std::left << std::setw(10) << recordA.name
+                << " | " << "NULL" << '\n';
+    }
+  }
+  std::cout << '\n';
+}
+
+void printRightJoin(const Table& left, const Table& right) {
+  std::cout << "RIGHT JOIN A x B ON A.id = B.id\n";
+  std::cout << "id | name_a     | name_b\n";
+  std::cout << "---+------------+----------\n";
+  for (const auto& recordB : sortedRecords(right)) {
+    if (auto recordA = left.get(recordB.id)) {
+      std::cout << std::right << std::setw(2) << recordB.id << " | " << std::left << std::setw(10) << recordA->name
+                << " | " << recordB.name << '\n';
+    } else {
+      std::cout << std::right << std::setw(2) << recordB.id << " | " << std::left << std::setw(10) << "NULL"
+                << " | " << recordB.name << '\n';
+    }
+  }
+  std::cout << '\n';
+}
+
 }  // namespace
 
 int main() {
@@ -71,6 +103,7 @@ int main() {
   printTable(*tableB, "Table B");
 
   printInnerJoin(*tableA, *tableB);
-
+  printLeftJoin(*tableA, *tableB);
+  printRightJoin(*tableA, *tableB);
   return 0;
 }
